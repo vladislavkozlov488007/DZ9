a04e9ceba67c1a71ebf4a5b52e3e194a53240862
vladislavkozlov488007 <117895583+vladislavkozlov488007@users.noreply.github.com>
2023-03-08 00:21:43 +0300

    Add files via upload

---
+++ z66.cs
@@ -0,0 +1,14 @@
+﻿// Задача 66: Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120; M = 4; N = 8. -> 30
+
+Console.Write("Введите натуральное число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите натуральное число n большее m: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"сумма натуральных элементов от {m} до {n} равна {SummaRec(m, n)}");
+
+int SummaRec(int m, int n)
+{
+    if (m == n) return m;
+    return (SummaRec(m, n - 1) + n);
+}
